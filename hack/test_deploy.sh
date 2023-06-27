425f350c1af12e8388b1dd3a2d2f711f23f28556
jmnote <opcore@gmail.com>
2023-06-27 01:42:46 +0000

    .

--- hack/test_deploy.sh
+++ hack/test_deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd $(dirname $0)/../
 set -x
-helm upgrade --install -n venti-stack venti-stack charts/venti-stack/ -f charts/venti-stack/values.yaml -f docs/values.test.yaml
-kubectl -n venti-stack delete $(kubectl get pod -n venti-stack -oname | grep venti)
+helm upgrade --install --create-namespace  -n venti-stack venti-stack charts/venti-stack/ -f charts/venti-stack/values.yaml -f docs/values.test.yaml
+
